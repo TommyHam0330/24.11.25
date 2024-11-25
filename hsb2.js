3c18949163e7013e0e4eb300a833a4209289c4f7
kjr6975 <kjr6975@gmail.com>
2024-11-25 15:56:13 +0900

    update

--- hsb2.js
+++ hsb2.js
@@ -4,7 +4,7 @@ class Food {
         this.name = name;
     }
     intro() {
-        document.write("제가 좋아하는 음식은 " + this.cate + "인 " + this.name + " 입니다. <br>");
+        document.write("제가 좋아하는 음식은 " + this.cate + "인 " + this.name + " 입니다. <br>" + "<hr>");
     }
 }
 let food1 = new Food("양식", "스파게티")
--- week12class.js
+++ week12class.js
@@ -5,7 +5,7 @@ class Phone {
     }
     
     intro() {
-        document.write("저는" + this.brand + "을 사용하고" + this.phonename + "입니다."+ "<br>");
+        document.write("저는" + this.brand + "을 사용하고" + this.phonename + "입니다."+ "<br>" + "<hr>");
     }
 }
 let phone1 = new Phone("아이폰", "iphoneX");
